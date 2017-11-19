aad9e0431e28907737700af9b52d3b0b626a63a6
masperbe <masperbe@asu.edu>
2017-11-19 17:16:41 -0500

    Error fixes and added/changed logic to "Enter" action listener.

--- src/GuessingGameUI.java
+++ src/GuessingGameUI.java
@@ -8,9 +8,9 @@ import javax.swing.border.EmptyBorder;
 import java.awt.GridLayout;
 import java.awt.GridBagLayout;
 import java.awt.FlowLayout;
-import com.jgoodies.forms.layout.FormLayout;
-import com.jgoodies.forms.layout.ColumnSpec;
-import com.jgoodies.forms.layout.RowSpec;
+//import com.jgoodies.forms.layout.FormLayout;
+//import com.jgoodies.forms.layout.ColumnSpec;
+//import com.jgoodies.forms.layout.RowSpec;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
 import java.util.Random;
@@ -19,9 +19,16 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class GuessingGameUI extends JFrame {
-
+	
 	private JPanel contentPane;
 	private JTextField textGuess;
+	
+	private final int MAX_LIVES = 5;
+	private final int MAX_NUMBER = 100;
+	private Random rand = new Random();
+	private int randomInt = rand.nextInt(MAX_NUMBER) + 1;
+	private int lives = MAX_LIVES;
+	private String guessStr = " ";
 
 	/**
 	 * Launch the application.
@@ -43,11 +50,6 @@ public class GuessingGameUI extends JFrame {
 	 * Create the frame.
 	 */
 	public GuessingGameUI() {
-		Random rand = new Random();
-		int randomInt = rand.nextInt(100) + 1;
-		int lives = 5;
-		String guessStr = " ";
-				
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
@@ -58,7 +60,6 @@ public class GuessingGameUI extends JFrame {
 		textGuess = new JTextField();
 		textGuess.setBounds(120, 167, 200, 22);
 		contentPane.add(textGuess);
-		textGuess.setColumns(10);
 		
 		JLabel lblText = new JLabel("Guess a number between 1-100. Lives remaining: " + lives);
 		lblText.setBounds(70, 13, 300, 16);
@@ -66,33 +67,31 @@ public class GuessingGameUI extends JFrame {
 		
 		JButton btnEnter = new JButton("Enter");
 		btnEnter.setBounds(145, 215, 150, 25);
-		contentPane.add(btnEnter);
-		if(btnEnter.isEnabled() == true){
-			btnEnter.addActionListener(new ActionListener(){
-				public void actionPerformed(ActionEvent e){
-					guessStr = lblText.getText();
+		btnEnter.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				guessStr = textGuess.getText();
+				
+				int guess = Integer.parseInt(guessStr);
+				if (guess == randomInt) {
+					lblText.setText("You won! Good job! Lives remaining " + lives);
+					btnEnter.setEnabled(false);
+				} else {
+					lives--;
+					if (lives == 0) {
+						lblText.setText("Sorry! You lost! Lives remaining: " + lives);
+						btnEnter.setEnabled(false);
+					} else if (randomInt - guess > 0 && randomInt - guess <= 5) {
+						lblText.setText("Your guess is just a little too low. Lives remaining " + lives);
+					} else if (randomInt - guess < 0 && randomInt - guess >= -5) {
+						lblText.setText("Your guess is just a little too high. Lives remaining " + lives);
+					} else if (randomInt - guess > 0) {
+						lblText.setText("Your guess is too low. Lives remaining " + lives);
+					} else if (randomInt - guess < 0) {
+						lblText.setText("Your guess is too high. Lives remaining " + lives);
+					}
 				}
-			});
-		}
-		int guess = Integer.parseInt(guessStr);
-		if(guess != randomInt && randomInt - guess > 0 && randomInt - guess <= 5){
-			lives--;
-			lblText.setText("Your guess is just a little too low. Lives remaining " + lives);
-		}
-		if(guess != randomInt && randomInt - guess < 0 && randomInt - guess >= -5){
-			lives--;
-			lblText.setText("Your guess is just a little too high. Lives remaining " + lives);
-		}
-		if(guess != randomInt && randomInt - guess > 0 ){
-			lives--;
-			lblText.setText("Your guess is too low. Lives remaining " + lives);
-		}
-		if(guess != randomInt && randomInt - guess < 0){
-			lives--;
-			lblText.setText("Your guess is too high. Lives remaining " + lives);
-		}
-		if(guess == randomInt){
-			lblText.setText("You won! Good job!");
-		}
+			}
+		});
+		contentPane.add(btnEnter);
 	}
 }
